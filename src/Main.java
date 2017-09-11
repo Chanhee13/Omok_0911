0b7b00d252a9155d6d77a5f0e5f0d83c73c64bd6
chanheepark <ch13p@naver.com>
2017-09-11 19:40:04 +0900

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
     public static void main(String[] args){
     	System.out.println("hello Omok");
-    	System.out.println("hello Omok");
-    	System.out.println("hello Omok");
+        PApplet.main("Main");
+    }
+    @Override
+    public void settings(){
+        this.size(600,600);
+    }
 
+    @Override
+    public void setup(){
+        this.background(0);
+    }
+
+    @Override
+    public void draw(){
+        this.background(248, 196, 126);
     }
 }
